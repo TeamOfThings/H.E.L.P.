2170516d9c7b5d4fc6785a06a942aefb6a00663b
0Alic <andrealisi.12lj@gmail.com>
2018-04-04 23:38:53 +0200

    merge

--- sniffer/test.py
+++ sniffer/test.py
@@ -1,21 +1,2 @@
 from bluepy.btle import Scanner, DefaultDelegate
 
-class ScanDelegate(DefaultDelegate):
-    def __init__(self):
-        DefaultDelegate.__init__(self)
-
-    def handleDiscovery(self, dev, isNewDev, isNewData):
-        if isNewDev:
-            print "Discovered device",  dev.addr,  " ", dev.rssi
-        elif isNewData:
-            print "Received new data from", dev.addr
-
-scanner = Scanner().withDelegate(ScanDelegate())
-
-while(True):
-	devices = scanner.scan(0.5)
-
-	#for dev in devices:
-	#    print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
-	#    for (adtype, desc, value) in dev.getScanData():
-	#	print "  %s = %s" % (desc, value)
