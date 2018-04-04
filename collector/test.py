be93bb85f21744ac8ff21a4a63836e223c188bf1
Chiara <baragliachiara@yahoo.it>
2018-04-04 21:57:07 +0200

    Aggiunto file di test per scansione

---
+++ collector/test.py
@@ -0,0 +1,21 @@
+from bluepy.btle import Scanner, DefaultDelegate
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if isNewDev:
+            print "Discovered device",  dev.addr,  " ", dev.rssi
+        elif isNewData:
+            print "Received new data from", dev.addr
+
+scanner = Scanner().withDelegate(ScanDelegate())
+
+while(True):
+	devices = scanner.scan(2.0)
+
+	for dev in devices:
+	    print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+	    for (adtype, desc, value) in dev.getScanData():
+		print "  %s = %s" % (desc, value)
