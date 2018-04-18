42aff2670cb7f7d44c16c661dd60b212852fce94
Chiara <baragliachiara@yahoo.it>
2018-04-18 22:31:59 +0200

    Inserito nel file di configurazione parametro per intervallo di scansione

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -6,45 +6,40 @@ import paho.mqtt.publish as publisher
 import json
 import sys
 
-
-devicesArray= ""
+devicesArray = []
 
 
 ### ####
 ### MAIN
-def main () :
-	scanner = None
-	json_data = None
-	
-	print ("Hello client!")
-	if len(sys.argv) != 2 :
-		sys.exit ("Wrong numer of arguments")
- 
-	print ("Initializing station..")
-	scanner = Scanner().withDelegate(ScanDelegate())
+def main():
+    scanner = None
+    json_data = None
 
-	json_data= json.load (open (sys.argv[1]))
-	
-	
-	#####*****#####
-	#####*****#####
+    print ("Hello client!")
+    if len(sys.argv) != 2:
+        sys.exit("Wrong numer of arguments")
 
-	
-	global devicesArray	
-	print ("Station ID is: " + json_data["id"])
-	devicesArray= json_data["devices"]
-	
-	while(True):
-		devices = scanner.scan(0.5)
-		## Test code..
-		#for dev in devices:
-		#	print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
-		#	for (adtype, desc, value) in dev.getScanData():
-		#		print "  %s = %s" % (desc, value)
-				
-				
-				
-				
+    print ("Initializing station..")
+    scanner = Scanner().withDelegate(ScanDelegate())
+
+    json_data = json.load(open(sys.argv[1]))
+
+    #####*****#####
+    #####*****#####
+
+    global devicesArray
+    print ("Station ID is: " + json_data["id"])
+    devicesArray = json_data["devices"]
+
+    while (True):
+        devices = scanner.scan(float(json_data["scan_interval"]))
+    ## Test code..
+
+
+# for dev in devices:
+#	print "Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType, dev.rssi)
+#	for (adtype, desc, value) in dev.getScanData():
+#		print "  %s = %s" % (desc, value)
 
 
 class ScanDelegate(DefaultDelegate):
@@ -53,29 +48,17 @@ class ScanDelegate(DefaultDelegate):
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
-        	for e in devicesArray :
-				if e["mac"] == dev.addr :
-					print "Name: ", e["name"], "\tRSSI: ", dev.rssi
-            		#publisher.single('hello', dev.rssi, hostname='192.168.1.75')
-       #elif isNewData:
-        #    print "Received new data from", dev.addr, " -> ", dev.rssi'''
-
-
+            for e in devicesArray:
+                if e["mac"] == dev.addr:
+                    print "Name: ", e["name"], "\tRSSI: ", dev.rssi
+        # publisher.single('hello', dev.rssi, hostname='192.168.1.75')
+    # elif isNewData:
+    #    print "Received new data from", dev.addr, " -> ", dev.rssi'''
 
 
 def on_connect(client, userdata, flags, rc):
     print('connected')
 
 
-
-
-
-	
-	
-	
-
-if __name__ == "__main__" :
-	main ()
-	
-	
-	
+if __name__ == "__main__":
+    main()
